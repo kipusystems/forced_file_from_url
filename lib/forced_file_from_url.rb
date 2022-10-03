4349e6888b3b1c5522616d6eb3f5730f1ee10149
Sampson Crowley <sampson.crowley@kipu.health>
2022-10-03 16:50:48 -0600

    feat: Only allow access to files through cleanup block

--- lib/forced_file_from_url.rb
+++ lib/forced_file_from_url.rb
@@ -24,26 +24,46 @@ module ForcedFileFromUrl
       file.rewind
     end
 
-    if block_given?
-      begin
-        yield file
-      ensure
-        file.close
-        file.unlink
-      end
-    else
-      file
-    end
+    _handle_ff_file(file, &block)
   end
 
   # Takes a paperclip attachment, downloads the file to tmp, and returns a file handle
-  def file_from_paperclip_attachment(attachment, style: :original)
+  def file_from_paperclip_attachment(attachment, style: :original, &block)
     filename = attachment.instance_read :file_name
     extname = File.extname filename
     basename = File.basename filename, extname
     file = Tempfile.new([basename, extname])
     attachment.copy_to_local_file style, file.path
     file.rewind
+
+    _handle_ff_file(file, &block)
+  end
+
+  def with_file_from_url_cleanup
+    yield if _within_ff_cleanup_block?
+    open_files = _open_ff_files
+    begin
+      @__block_open = true
+      yield
+    ensure
+      @open_files = []
+      @__block_open = false
+      open_files.each {|file|
+        file.close
+        file.unlink
+      }
+    end
+  end
+
+  def _open_ff_files
+    @open_files ||= []
+  end
+
+  def _within_ff_cleanup_block?
+    @__block_open
+  end
+
+  def _handle_ff_file(file, &block)
     if block_given?
       begin
         yield file
@@ -51,8 +71,14 @@ module ForcedFileFromUrl
         file.close
         file.unlink
       end
-    else
+    elsif _within_ff_cleanup_block?
+      # even if cleanup is forgotten it can be done on the next batch :evilsmile:
+      _open_ff_files << file
       file
+    else
+      file.close
+      file.unlink
+      raise ArgumentError, "unsafe file cleanup practice detected!"
     end
   end
 end
