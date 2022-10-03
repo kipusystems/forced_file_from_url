b02d24a855bfc7c5df02be8ab472863f7f36807b
Sampson Crowley <sampson.crowley@kipu.health>
2022-10-03 16:04:46 -0600

    fix: Allow block-form access to file urls

--- lib/forced_file_from_url.rb
+++ lib/forced_file_from_url.rb
@@ -6,26 +6,53 @@ module ForcedFileFromUrl
 
   # Using open-uri to open files that are less than 10kb will return a StringIO object.
   # This causes a problem when trying to call #path on the object which is not available
-  # in StringIO objects but rather File and Tempfile objects. This method writes the data 
+  # in StringIO objects but rather File and Tempfile objects. This method writes the data
   # to a Tempfile when the data is a StringIO.
   def forced_file_from_url(url)
+    file = nil
+
     data = open URI.parse(url)
-    return data if data.is_a? Tempfile
+    if data.is_a? Tempfile
+      file = data
+    else
+      extname = File.extname url
+      basename = File.basename url, extname
 
-    extname = File.extname url
-    basename = File.basename url, extname
+      file = Tempfile.new [basename, extname]
+      file.binmode
+      file.write data.read
+      file.rewind
+    end
 
-    file = Tempfile.new [basename, extname]
-    file.binmode
-    file.write data.read
-    file.rewind
-    file
+    if block_given?
+      begin
+        yield file
+      ensure
+        file.close
+        file.unlink
+      end
+    else
+      file
+    end
   end
 
   # Takes a paperclip attachment, downloads the file to tmp, and returns a file handle
   def file_from_paperclip_attachment(attachment, style: :original)
-    url = Rails.root.join("/tmp/#{attachment.instance_read :file_name}").to_s
-    attachment.copy_to_local_file style, url
-    File.open url, ?r
+    filename = attachment.instance_read :file_name
+    extname = File.extname filename
+    basename = File.basename filename, extname
+    file = Tempfile.new([basename, extname])
+    attachment.copy_to_local_file style, file.path
+    file.rewind
+    if block_given?
+      begin
+        yield file
+      ensure
+        file.close
+        file.unlink
+      end
+    else
+      file
+    end
   end
 end
